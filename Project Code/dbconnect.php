9e2daed3eee3951f6d6728c89d8407a69bada5ec
K. M. Muhaiminul Islam <aljamikhan9@gmail.com>
2025-09-29 22:14:06 +0600

    Update dbconnect.php

--- Project Code/dbconnect.php
+++ Project Code/dbconnect.php
@@ -2,7 +2,7 @@
 $DB_HOST = "localhost";
 $DB_USER = "root";
 $DB_PASS = "";
-$DB_NAME = "exchatloan";
+$DB_NAME = "microempower";
 
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 if ($mysqli->connect_errno) { die("DB connect failed: " . $mysqli->connect_error); }
@@ -10,3 +10,4 @@ $mysqli->set_charset("utf8mb4");
 
 if (session_status() === PHP_SESSION_NONE) session_start();
 ?>
+
